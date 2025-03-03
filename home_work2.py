6927593d3074591125dd6f2e110128e461f1bab9
Olga Mirishnichenko <ollf72@mail.ru>
2025-03-03 15:27:53 +0500

    ДЗ 2 fix

--- home_work2.py
+++ home_work2.py
@@ -1,45 +1,20 @@
 from datetime import time
 
-
-
 def test_dark_theme_by_time():
-    """
-    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
-    """
     current_time = time(hour=23)
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if current_time.hour <= 22 or current_time.hour > 6:
-        is_dark_theme = True
-    else:
-        is_dark_theme = False
-
+    is_dark_theme =22 <= current_time.hour or current_time.hour<6
     assert is_dark_theme is True
-
-test_dark_theme_by_time()
 
 def test_dark_theme_by_time_and_user_choice():
-    """
-    Протестируйте правильность переключения темной темы на сайте
-    в зависимости от времени и выбора пользователя
-    dark_theme_enabled_by_user = True - Темная тема включена
-    dark_theme_enabled_by_user = False - Темная тема выключена
-    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
-    """
+
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
-    if current_time.hour==16:
-        is_dark_theme=True
-    elif current_time.hour!=16:
-        is_dark_theme=False
+    if dark_theme_enabled_by_user is not None:
+        is_dark_theme = dark_theme_enabled_by_user
     else:
-        is_dark_theme=None
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
-
+        is_dark_theme = 22 <= current_time.hour or current_time.hour < 6
 
     assert is_dark_theme is True
-
-
 
 
 def test_find_suitable_user():
